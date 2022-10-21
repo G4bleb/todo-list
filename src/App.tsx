5cbde6b210389e90a3c55467ca79f2b268358da4
Gableb <gabriel.lebis@free.fr>
2022-10-21 15:07:53 +0200

    next iteration : crossing out items

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,44 @@ import React, { useState } from "react";
 import { addItemForm } from "./AddItemForm";
 import "./App.css";
 import { itemsList } from "./ItemsList";
+import { Item } from "./interfaces/Item";
+import { v4 as uuid } from "uuid";
+
+const startingArr: Item[] = [
+  { name: "item 1", crossedOut: false },
+  { name: "item 2", crossedOut: false },
+];
+
+const startingItems: Map<string, Item> = new Map();
+startingArr.forEach((item) => {
+  startingItems.set(uuid(), item);
+});
 
 function App() {
-  const [items, setItems] = useState(["item 1", "item 2"]);
+  const [items, setItems] = useState(startingItems);
+  const onNewItem = (newItemName: string) => {
+    const newMap = new Map(items);
+    newMap.set(uuid(), { name: newItemName, crossedOut: false });
+    setItems(newMap);
+  };
+  const onItemClick = (id: string) => {
+    const newItem = items.get(id) as Item;
+    newItem.crossedOut = !newItem.crossedOut;
+    const newMap = new Map(items);
+    newMap.set(id, newItem);
+    setItems(newMap);
+  };
+
+  console.log(JSON.stringify(Array.from(items.entries()), null, 2));
+
   return (
     <div className="App">
-      {itemsList({ items })}
+      {itemsList({
+        items,
+        onItemClick: onItemClick,
+      })}
       {addItemForm({
-        onSubmit: (newItem) => {
-          setItems([...items, newItem]);
-        },
+        onSubmit: onNewItem,
       })}
     </div>
   );
--- src/ItemsList.tsx
+++ src/ItemsList.tsx
@@ -1,9 +1,25 @@
-export function itemsList({ items }: { items: string[] }) {
-  return (
-    <ul>
-      {items.map((i, index) => (
-        <li key={index}>{i}</li>
-      ))}
-    </ul>
-  );
+import { Item } from "./interfaces/Item";
+
+export function itemsList({
+  items,
+  onItemClick,
+}: {
+  items: Map<string, Item>;
+  onItemClick(id: string): void;
+}) {
+  const elems: JSX.Element[] = [];
+  items.forEach((item, id) => {
+    elems.push(
+      <li
+        onClick={() => {
+          onItemClick(id);
+        }}
+        key={id}
+        style={item.crossedOut ? { textDecoration: "line-through" } : {}}
+      >
+        {item.name}
+      </li>
+    );
+  });
+  return <ul>{elems}</ul>;
 }

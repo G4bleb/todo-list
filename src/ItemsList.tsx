5d17127b9feeee88e3af0dd0cd215bed3258de74
Gableb <gabriel.lebis@free.fr>
2022-10-21 16:59:31 +0200

    cleanup

--- src/ItemsList.tsx
+++ src/ItemsList.tsx
@@ -13,7 +13,7 @@ export function itemsList({
   items.forEach((item, id) => {
     elems.push(
       <li
-        className="border-t border-t-gray-300 first:border-t-0 hover:bg-gray-100 p-2 flex content-between justify-between select-none hover:cursor-pointer"
+        className="border-t border-t-gray-300 first:border-t-0 first:rounded-t-lg last:rounded-b-lg hover:bg-gray-100 p-2 flex content-between justify-between select-none hover:cursor-pointer"
         onClick={() => {
           onItemClick(id);
         }}
